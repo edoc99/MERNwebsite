645a6f035327d6a3d0a518fb07a196c4040e31ce
edoc99 <harshitjainf3@gmail.com>
2019-12-03 00:53:13 +0530

    Moduleraize express

--- server/index.js
+++ server/index.js
@@ -1,26 +1,18 @@
-const express = require('express')
+const express = require('express');
 
-const app = express()
-const port = 4000
+const handle = require('./handlers')
+
+const app = express();
+const port = 4000;
 
 // routes
 app.get('/', function (req, res) {
   res.json({name: "John", 
   age: 31, city: "New York"})
-})
+});
 
 // error handler
-app.use((req, res, next) =>{
-    const err = new Error('Not found');
-    err.status = 404;
-
-    next(err);
-})
-
-app.use((err, req, res, next) => {
-    res.status(err.status || 500).json({
-        err: err.message || 'Something went wrong'
-    })
-})
+app.use(handle.notFound);
+app.use(handle.errors);
  
-app.listen(port, console.log(`Server started on port ${port}`))
+app.listen(port, console.log(`Server started on port ${port}`));
