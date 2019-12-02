5cdd60e66dc27fcc201df811fe7b1dfca04bac9b
edoc99 <harshitjainf3@gmail.com>
2019-12-03 01:19:02 +0530

    Added more dependencies and modularize express

--- server/index.js
+++ server/index.js
@@ -1,9 +1,15 @@
+require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
 const handle = require('./handlers')
 
 const app = express();
-const port = 4000;
+const port = process.env.PORT;
+
+app.use(cors());
+app.use(bodyParser.json());
 
 // routes
 app.get('/', function (req, res) {
