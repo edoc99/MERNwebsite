cedeb43b7fc02991dfbedf618c9bf57102361d62
edoc99 <harshitjainf3@gmail.com>
2019-12-03 03:30:33 +0530

    created encryption and comparison function

--- server/models/user.js
+++ server/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
     username: {
@@ -15,5 +16,29 @@ const userSchema = new mongoose.Schema({
         default: Date.now,
     }
 });
+
+// encrypting the password before saving to the database
+userSchema.pre('save', async function (next) {
+   try {
+    if (!this.isModified('password')) {
+        return next();
+    }
+
+    const hashed = await bcrypt.hash(this.password, 10);
+    this.password = hashed;
+    return next();
+   } catch (err) {
+       return next(err)
+   }
+});
+
+//compairing password
+userSchema.methods.comparePassword = async function(attempt, next){
+    try {
+        return await bcrypt.compare(attempt, this.password);
+    } catch(err) {
+        next(err);
+    }
+}
 
 module.exports = mongoose.model('User', userSchema);
