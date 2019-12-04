da92ce33834d7bbbe01b8ca1e3b3419332a09dd9
edoc99 <harshitjainf3@gmail.com>
2019-12-04 20:30:22 +0530

    Handling errors in React

--- client/src/containers/App.jsx
+++ client/src/containers/App.jsx
@@ -1,10 +1,11 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {Provider} from 'react-redux';
 import decode from 'jwt-decode'
 
 import { store } from '../store';
 import { setCurrentUser, addError, setToken } from '../store/actions'
 import Auth from '../components/Auth';
+import ErrorMessage from '../components/ErrorMessage';
 
 if (localStorage.jwtToken) {
   setToken(localStorage.jwtToken);
@@ -19,6 +20,7 @@ if (localStorage.jwtToken) {
 const App = () => (
     <Provider store={store}>
         <Auth authType={'login'} />
+        <ErrorMessage />
      </Provider>        
 );
 
--- client/src/store/reducers/error.js
+++ client/src/store/reducers/error.js
@@ -1,7 +1,7 @@
 import {ADD_ERROR, REMOVE_ERROR} from '../actionTypes';
 
 export default (state = {}, action) => {
-    switch(action.types) {
+    switch(action.type) {
         case ADD_ERROR:
             return {...state, message: action.error };
 
